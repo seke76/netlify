d9cc70ca68183b7846abf51302003e3f2b79cb37
Stefan Eklund <stefan.eklund76@gmail.com>
2020-12-27 13:09:00 +0100

    loggning

--- functions/app.js
+++ functions/app.js
@@ -1,6 +1,9 @@
 exports.handler = async function(event, context) {
+    console.log("Fjast");
     return {
         statusCode: 200,
         body: JSON.stringify({message: "Hello World"})
     };
 }
+
+console.log("bottom!");
